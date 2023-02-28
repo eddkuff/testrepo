68fb083ce3be7995dc63f1d60371810ee4de341f
eddkuff <eddkuff@hotmail.com>
2023-02-28 11:34:51 -0800

    Add files via upload

---
+++ fortest.py
@@ -0,0 +1,17 @@
+
+#def main():
+#    i = 1
+#    max = 10
+#    while (i < max):
+#        print(i)
+#        i = i + 1
+#
+# main()
+
+
+
+if 100 > 99 and \
+    200 <= 300 and \
+    True != False:
+        print('Hello World!')
+
---
+++ strings.py
@@ -0,0 +1,22 @@
+# Strings in python are surrounded by either single or double quotation marks. Let's look at string formatting and some string methods
+
+name = 'Brad'
+age = 37
+
+# Concatenate
+print('Hello, my name is ' + name + ' and I am ' + str(age))
+
+
+# String Formatting
+# Arguents by position
+# print('My name is {name} and I am {age}'.format(name=name,age=age))
+
+#F-Strings (3.6+)
+# print(f'Hello, my name is {name} and I am {age}')
+# String Methods
+
+
+# Capitalize string
+
+s = 'hello world'
+print(s.lower())
